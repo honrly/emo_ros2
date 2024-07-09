a583043d3987914adbc70524053cab9065cb7707
honrly <hondarika0322@gmail.com>
2024-07-09 22:31:53 +0900

    change emo_status emo_status_rest_base

--- setup.py
+++ setup.py
@@ -25,13 +25,9 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-		'pnnx = emo_voice.pnnx:main', 
-        'move = emo_voice.move:main', 
-        'plot = emo_voice.plot:main', 
-        'client = emo_voice.client:main', 
-        'server = emo_voice.server_voice:main', 
         'brain_wave = emo_voice.brain_wave:main', 
-        'emo_status = emo_voice.emo_status:main', 
+        'emo_status_rest_base = emo_voice.emo_status_rest_base:main', 
+        'visualizer = emo_voice.visualizer:main', 
         ],
     },
 )
